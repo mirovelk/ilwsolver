a9a07ca2daba1535811bdf96f6d9f707b4827079
Miroslav Velk <miro.velk@gmail.com>
2022-01-31 14:04:17 +0100

    support M>1

--- src/support/calc/calc.test.tsx
+++ src/support/calc/calc.test.tsx
@@ -4,7 +4,24 @@ test("eqns returns correct value", () => {
   expect(eqns([[0.5, 0.25]], [0.5, 0.25])).toEqual([[9.28125, 5.921875]]);
 });
 
-test("eqnsd returns correct value", () => {
+test("eqns returns correct value for M=3", () => {
+  expect(
+    eqns(
+      [
+        [0.5, 0.2],
+        [0.4, 0.3],
+        [0.3, 0.4],
+      ],
+      [0.5, 0.25]
+    )
+  ).toEqual([
+    [11905.258487440005, 3175.397525280002],
+    [8532.47024318, 5601.463081760001],
+    [5322.1174444, 8279.692435200002],
+  ]);
+});
+
+test("eqnsd returns correct value for M=1", () => {
   expect(eqnsd([[0.5, 0.25]], [0.5, 0.25])).toEqual([[[-1.125, -2.75]]]);
 });
 
--- src/support/calc/calc.ts
+++ src/support/calc/calc.ts
@@ -12,25 +12,38 @@ const E1 = complex(2);
 const E2 = complex(3);
 const E3 = complex(-5);
 
-let N: number = 2;
-const AL: Complex[] = [complex(6), complex(5)];
-const AR: Complex[] = [complex(3), complex(2)];
-
-let M: number = 1;
+const AL: Complex[] = [complex(6), complex(5)]; // length N
+const AR: Complex[] = [complex(3), complex(2)]; // length N
 
 // input vector M, output vector M
 export function eqns(input: Complex[], q: Complex): Complex[] {
-  let tmp1 = copy(q);
-  let tmp2 = complex(1);
+  let result: Complex[] = [];
+  for (let i = 0; i < input.length; i++) {
+    let tmp1 = copy(q);
+    let tmp2 = complex(1);
 
-  for (let i = 0; i < AL.length; i++) {
-    tmp1 = multiply(tmp1, subtract(input[0], AL[i]));
-    tmp2 = multiply(tmp2, subtract(input[0], AR[i]));
+    for (let j = 0; j < AL.length; j++) {
+      tmp1 = multiply(tmp1, subtract(input[i], AL[j]));
+      tmp2 = multiply(tmp2, subtract(input[i], AR[j]));
+    }
+
+    for (let j = 0; j < input.length; j++) {
+      if (j !== i) {
+        tmp1 = multiply(tmp1, add(subtract(input[i], input[j]), E1));
+        tmp2 = multiply(tmp2, subtract(subtract(input[i], input[j]), E1));
+
+        tmp1 = multiply(tmp1, add(subtract(input[i], input[j]), E2));
+        tmp2 = multiply(tmp2, subtract(subtract(input[i], input[j]), E2));
+
+        tmp1 = multiply(tmp1, add(subtract(input[i], input[j]), E3));
+        tmp2 = multiply(tmp2, subtract(subtract(input[i], input[j]), E3));
+      }
+    }
+
+    result = [...result, subtract(tmp1, tmp2)];
   }
 
-  const result = subtract(tmp1, tmp2);
-
-  return [result];
+  return result;
 }
 
 // input vector M, output matrix MxM
