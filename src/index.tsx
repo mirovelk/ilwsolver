fe9d436200ad7dba924b471918e216e7764e2d16
Miroslav Velk <miro.velk@gmail.com>
2023-03-04 19:57:04 +0100

    enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 import { Provider as StoreProvider } from 'react-redux';
+import { RecoilRoot } from 'recoil';
 
 import App from './App';
 import store from './redux/store';
@@ -14,9 +16,12 @@ const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 
-// TODO re-enable strict mode
 root.render(
-  <StoreProvider store={store}>
-    <App />
-  </StoreProvider>
+  <React.StrictMode>
+    <StoreProvider store={store}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </StoreProvider>
+  </React.StrictMode>
 );
